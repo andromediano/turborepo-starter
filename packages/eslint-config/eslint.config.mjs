c433523713734664f4acde151e0e91ef6eea63b2
andromediano <andromediano@gmail.com>
2025-04-01 23:39:12 +0900

    Update eslint configurations

--- packages/eslint-config/eslint.config.mjs
+++ packages/eslint-config/eslint.config.mjs
@@ -1,8 +1,8 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import tanstackQueryPlugin from '@tanstack/eslint-plugin-query';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import tanstackQueryPlugin from "@tanstack/eslint-plugin-query";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,62 +14,73 @@ const compat = new FlatCompat({
 const eslintConfig = [
   eslintPluginPrettier,
   {
-    files: ['**/*.{js,jsx,ts,tsx}'], // 적용 대상 파일 명시
+    files: ["**/*.{js,jsx,ts,tsx}"], // 적용 대상 파일 명시
     plugins: {
-      '@tanstack/query': tanstackQueryPlugin,
+      "@tanstack/query": tanstackQueryPlugin,
     },
     rules: {
       ...tanstackQueryPlugin.configs.recommended.rules, // 추천 규칙 적용
     },
   },
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
+    extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/jsx-props-no-spreading': 'off',
-      'react/require-default-props': 'off',
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/require-default-props": "off",
       // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
-      'react/function-component-definition': [
-        'error',
+      "react/function-component-definition": [
+        "error",
         {
-          namedComponents: ['arrow-function', 'function-declaration'],
-          unnamedComponents: 'function-expression',
+          namedComponents: ["arrow-function", "function-declaration"],
+          unnamedComponents: "function-expression",
         },
       ],
       // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/main/docs/rules/label-has-associated-control.md
-      'jsx-a11y/label-has-associated-control': [
-        'error',
+      "jsx-a11y/label-has-associated-control": [
+        "error",
         {
-          assert: 'either',
+          assert: "either",
         },
       ],
-      'import/prefer-default-export': 'off',
-      'import/no-extraneous-dependencies': ['warn', { bundledDependencies: false }],
-      'import/order': [
-        'error',
+      "import/prefer-default-export": "off",
+      "import/no-extraneous-dependencies": [
+        "warn",
+        { bundledDependencies: false },
+      ],
+      "import/order": [
+        "error",
         {
-          'newlines-between': 'never',
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'unknown'],
+          "newlines-between": "never",
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "unknown",
+          ],
           pathGroups: [
             {
-              pattern: 'react',
-              group: 'builtin',
-              position: 'before',
+              pattern: "react",
+              group: "builtin",
+              position: "before",
             },
             {
-              pattern: 'next/*',
-              group: 'builtin',
-              position: 'before',
+              pattern: "next/*",
+              group: "builtin",
+              position: "before",
             },
           ],
           pathGroupsExcludedImportTypes: [],
           alphabetize: {
-            order: 'asc',
+            order: "asc",
           },
         },
       ],
-      'no-plusplus': 'off',
+      "no-plusplus": "off",
     },
   }),
 ];
